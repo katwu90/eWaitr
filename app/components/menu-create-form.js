ba0a4905a7711a63d1cc6c60e75d5f8d594061b0
katwu90 <katwu90@gmail.com>
2017-11-02 13:01:02 -0400

    Adds createMenu action
    Adds createMenu action in menu-create-form and the action is sent up to
    menus template and js file. Users can successfully create a new menu.

--- app/components/menu-create-form.js
+++ app/components/menu-create-form.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  menu: {
+    title: null,
+    notes: null
+  },
+  actions: {
+    createMenu () {
+      this.sendAction('createMenu', this.get('menu'));
+      this.set('menu.title', null);
+      this.set('menu.notes', null);
+    }
+  }
 });
--- app/routes/menus.js
+++ app/routes/menus.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('menu');
+  },
+  actions: {
+    createMenu (menu) {
+      let newMenu = this.get('store').createRecord('menu', menu);
+      newMenu.save();
+    }
   }
 });
