f1bbf852f5830af58efcd496a6cb313b94ccd52e
katwu90 <katwu90@gmail.com>
2017-11-02 11:49:34 -0400

    Changes menu route and title of app web page
    - changes the route of menu to `menus/:menu_id`
    - changes the title of the app to eWaitr

--- app/router.js
+++ app/router.js
@@ -9,8 +9,8 @@ Router.map(function () {
   this.route('sign-up');
   this.route('sign-in');
   this.route('change-password');
-  // this.route('users');
   this.route('menus');
+  this.route('menu', { path: '/menus/:menu_id'});
 });
 
 export default Router;
