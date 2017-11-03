d045dab8bb1da918addc7b7a9ab0152e50bba20d
katwu90 <katwu90@gmail.com>
2017-11-02 20:12:02 -0400

    Can create new menu-items in a menu
    - Add form fields to menu-item-create-form.hbs file
    - Linked the value of each form field to the object in the js file.
    - Sends create new menu item action up to the menu .hbs file and sends a
      POST request to API in menu.js file.

--- app/components/menu-item-create-form.js
+++ app/components/menu-item-create-form.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  menuItem: {
+    name: null,
+    price: null,
+    description: null,
+    foodCategory: null
+  },
+  actions: {
+    createMenuItem () {
+      this.get('menuItem').menu = this.get('menu');
+      console.log(this.get('menuItem'));
+      this.sendAction('createMenuItem', this.get('menuItem'));
+      this.set('menuItem.name', null);
+      this.set('menuItem.price', null);
+      this.set('menuItem.description', null);
+      this.set('menuItem.foodCategory', null);
+    }
+  }
 });
--- app/routes/menu.js
+++ app/routes/menu.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('menu', params.menu_id);
+  },
+  actions: {
+    createMenuItem (menuItem) {
+      let newMenuItem = this.get('store').createRecord('menuItem', menuItem);
+      newMenuItem.save();
+    }
   }
 });
